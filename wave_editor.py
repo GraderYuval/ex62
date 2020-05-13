13f2ba2a31f4fc8b11c08e1cc35038a65a71c568
GraderYuval <42010770+GraderYuval@users.noreply.github.com>
2020-05-13 16:05:17 +0300

    Initial commit

---
+++ wave_editor.py
@@ -0,0 +1,127 @@
+from wave_helper import *
+
+# messages for user
+ENTRY_MSG = "WELCOME: press the number of the option: \n" \
+            "1.edit wav \n2.compose wav \n3.abort program "
+WAV__FILENAME_REQUEST = "write down wav filename"
+WRONG_WAVE_FILENAME = "wave file name was incorrect"
+CHANGING_WAVE_MENU_MSG = "File edit menu:press the number of the option: \n " \
+                         " 1. reverse wav \n        2. audio off \n" \
+                         " 3. increase wav volume \n4. increase wav speed\n " \
+                         " 5.decrease wav volume \n 6.  decrease_wav_speed \n" \
+                         " 7.low path filter \n     8.  move to end menu"
+MENU_INPUT_INCORRECT = "your menu input was incorrect"
+
+
+# help functions
+def display(msg):
+    print(msg)
+
+
+def get_menu_input(input_option, wrong_input_msg):
+    user_input = input()
+    if int(user_input) in input_option:
+        return user_input
+    else:
+        display(wrong_input_msg)
+        get_menu_input(input_option, wrong_input_msg)
+
+
+# edit functions
+def reverse_wav(file_name):
+    pass
+
+
+def audio_off(file_name):
+    pass
+
+
+def increase_wav_speed(file_name):
+    pass
+
+
+def decrease_wav_speed(file_name):
+    pass
+
+
+def increase__wav_volume(file_name):
+    pass
+
+
+def decrease__wav_volume(file_name):
+    pass
+
+
+def low_path_filter(file_name):
+    pass
+
+
+def load_wave_file():
+    while True:
+        display(WAV__FILENAME_REQUEST)
+        file_name = input()
+        load_file = load_wave(file_name)
+        if load_file != -1:
+            return load_file, file_name
+        else:
+            display(WRONG_WAVE_FILENAME)
+
+
+def call_edit_function(user_input, wave_file):
+    if user_input == 1:
+        return reverse_wav()
+    elif user_input == 2:
+        return audio_off()
+    elif user_input == 3:
+        return increase__wav_volume()
+    elif user_input == 4:
+        return increase_wav_speed()
+    elif user_input == 5:
+        return decrease__wav_volume()
+    elif user_input == 6:
+        return decrease_wav_speed()
+    elif user_input == 7:
+        return low_path_filter()
+    elif user_input == 8:
+        return "8"
+
+
+def end_menu(file_name):
+    
+
+
+def changing_wav_menu():
+    wave_file, file_name = load_wave_file()
+    in_edit_mode = True
+    while in_edit_mode:
+        display(CHANGING_WAVE_MENU_MSG)
+        user_input = get_menu_input({1, 2, 3, 4, 5, 6, 7, 8}
+                                    , MENU_INPUT_INCORRECT)
+        wave_file = call_edit_function(user_input, wave_file)
+        if wave_file is "8":  # exit this menu
+            in_edit_mode = False
+        else:  # save wave file and another iteration
+            save_wave(wave_file[0], wave_file[1], file_name)
+    end_menu()
+
+
+def composition():
+    pass
+
+
+def entry_menu():
+    display(ENTRY_MSG)
+    user_input = get_menu_input({1, 2, 3}, MENU_INPUT_INCORRECT)
+    if user_input == 1:
+        changing_wav_menu()
+    if user_input == 2:
+        composition()
+    if user_input == 3:
+        end_menu()
+
+
+def main_function():
+    entry_menu()
+
+
+main_function()
